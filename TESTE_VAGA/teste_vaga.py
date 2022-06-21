92056c6e8152d7c66d67c33ff03e46e3d79ddc70
Caslu-Faria <lucasglf.contato@gmail.com>
2022-06-21 15:22:38 -0300

    preencimento campo cep

--- TESTE_VAGA/teste_vaga.py
+++ TESTE_VAGA/teste_vaga.py
@@ -1,4 +1,8 @@
+from tokenize import String
 from selenium import webdriver
 
 driver=webdriver.Chrome(executable_path=r'./chromedriver.exe')
+
 driver.get('https://buscacepinter.correios.com.br/app/endereco/index.php')
+driver.find_element_by_xpath('//*[@id="endereco"]').send_keys('04547-130')  
+
